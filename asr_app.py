6ece59abdcc53fd7c3c853c8500b6c0d7500d1ec
pythonista0801 <78151237+pythonista0801@users.noreply.github.com>
2022-01-18 11:46:09 +0200

    Update asr_app.py

--- asr_app.py
+++ asr_app.py
@@ -3,13 +3,15 @@ import librosa
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import streamlit as st
 
+st.header("Trascribe Russian Audio")
+
 uploaded_file = st.file_uploader(label = "Please upload your file" )
 
 LANG_ID = "ru"
 MODEL_ID = "jonatasgrosman/wav2vec2-large-xlsr-53-russian"
 
 #load wav2vec2 tokenizer and model
-tokenizer = Wav2Vec2Tokenizer.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-russian")
+processor = Wav2Vec2Processor.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-russian")
 
 model = Wav2Vec2ForCTC.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-russian")
 
@@ -26,11 +28,11 @@ def asr_transcript(audio_file):
         if len(speech.shape) > 1:
             speech = speech[:, 0] + speech[:, 1]
 
-        input_values = tokenizer(speech, return_tensors="pt").input_values
-        logits = model(input_values).logits
+        inputs = processor(speech, sampling_rate=16_000, return_tensors="pt", padding=True)
+        logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
 
         predicted_ids = torch.argmax(logits, dim=-1)
-        transcription = tokenizer.batch_decode(predicted_ids)[0]
+        transcription = processor.batch_decode(predicted_ids)
         transcript += transcription.lower() + " "
 
     return transcript
